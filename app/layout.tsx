25a4357743f77da87c8754704075714fd94ab9d9
Daniil Oberlev <denioberlev@gmail.com>
2025-11-16 01:28:47 +0300

    feat(analytics): add heatmap analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,29 @@ import './styles/globals.css';
 
 import type React from 'react';
 
+import Script from 'next/script';
+
 import { ThemeProvider } from '@/features/theme-toggle/lib/ThemeProvider';
 import { Providers } from '@/providers';
 import { Footer, Header } from './widgets';
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => (
   <html lang='ru' suppressHydrationWarning>
+    <head>
+      <Script
+        id='microsoft-clarity'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u6p7w70630");
+          `
+        }}
+      />
+    </head>
     <body>
       <Providers>
         <ThemeProvider
